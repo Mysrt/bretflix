acae6be43aab82b1db8166f709f98fd52f932017
Jimmy Trowbridge <trowbridge.james@gmail.com>
2011-12-23 13:42:43 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bretflix::Application.config.secret_token = '98c444e258224d435eb66aa21599ca6994904e600d3e954bbf299487fe7c9406e9eb8bf76694f6f19779c0063203658b8cfd4ad2ca90fa8153af3853533516c3'
